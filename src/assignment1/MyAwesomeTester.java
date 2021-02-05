3c29576cdd11965fa0e37d19dc03877003ef3a4c
Sasha, from this demo! <alexander.aleshchenko@mail.mcgill.ca>
2021-02-05 12:19:25 -0800

    :sparkels: initial commit :sparkles:

---
+++ src/assignment1/MyAwesomeTester.java
@@ -0,0 +1,10 @@
+package assignment1;
+
+public class MyAwesomeTester {
+	public static void main(String[] args) {
+		Basket basket = new Basket();
+		basket.add(new Egg("steve",3,3));
+		System.out.println(basket.getTotalTax());
+	}
+
+}
